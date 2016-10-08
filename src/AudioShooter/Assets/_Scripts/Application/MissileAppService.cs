4e93fd48f4eb5c3c79849518ef6135789fc95f43
Diego Giacomelli <giacomelli@gmail.com>
2016-10-08 11:35:33 -0300

    Created MissileAppService.

---
+++ src/AudioShooter/Assets/_Scripts/Application/MissileAppService.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class MissileAppService 
+{
+	private static Object _missilePrefab = Resources.Load("MissilePrefab");
+
+	public static GameObject CreateMissile(GameObject shooter, Vector3 position, Vector3 direction)
+	{
+		var go = (GameObject) GameObject.Instantiate(_missilePrefab, position, Quaternion.identity);
+		var controller = go.GetComponent<MissileController>();
+		controller.Direction = direction;
+		controller.Shooter = shooter;
+
+		return go;
+	}
+
+}
