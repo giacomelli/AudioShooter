d580d84e91ce0d2ac3668907b22cd4e566c7b720
Diego Giacomelli <giacomelli@gmail.com>
2016-10-08 11:34:27 -0300

    Create some collider extensions.

---
+++ src/AudioShooter/Assets/_Scripts/Extensions/ColliderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ColliderExtensions {
+
+	public static bool IsEnemy(this Collider collider)
+	{
+		return collider.tag == "Enemy";
+	}
+
+	public static bool IsMountain(this Collider collider)
+	{
+		return collider.tag == "Mountain";
+	}
+
+	public static bool IsMissile(this Collider collider)
+	{
+		return collider.tag == "Missile";
+	}
+
+	public static bool IsMyMissile(this Collider otherCollider, MonoBehaviour me)
+	{
+		if (otherCollider.IsMissile())
+		{
+			var missileController = otherCollider.GetComponent<MissileController>();
+
+			return missileController.Shooter == me.gameObject;
+		}
+
+		return false;
+	}
+}
