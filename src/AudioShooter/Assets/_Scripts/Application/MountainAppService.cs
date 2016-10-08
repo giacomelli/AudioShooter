bb381c6361da8cf91b9760d1595d2913c0bc13a1
Diego Giacomelli <giacomelli@gmail.com>
2016-10-08 11:35:47 -0300

    Created MountainAppService.

--- src/AudioShooter/Assets/_Scripts/Application/MountainAppService.cs
+++ src/AudioShooter/Assets/_Scripts/Application/MountainAppService.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class MountainAppService 
 {
@@ -8,5 +10,13 @@ public static class MountainAppService
 	public static GameObject CreateMountain(Vector3 position)
 	{
 		return (GameObject) GameObject.Instantiate(_mountainPrefab, position, Quaternion.identity);
+	}
+
+	public static GameObject[] GetNewestMountains(int limit)
+	{
+		return GameObject.FindGameObjectsWithTag("Mountain")
+			             .OrderByDescending(m => m.transform.position.z)
+			             .Take(limit)
+			             .ToArray();
 	}
 }
