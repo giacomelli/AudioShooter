d7107adebee42de1c251e498abaea52869f10df4
Diego Giacomelli <giacomelli@gmail.com>
2016-10-07 23:13:13 -0300

    CameraController started.

---
+++ src/AudioShooter/Assets/_Scripts/Controllers/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float _velocity;
+
+	void Update () {
+		var target = new Vector3(transform.position.x, transform.position.y, ScenarioDeployer.CurrentZ);
+		transform.position = Vector3.Lerp(transform.position, target, _velocity);
+	}
+}
