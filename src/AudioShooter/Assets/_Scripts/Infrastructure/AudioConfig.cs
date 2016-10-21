21f24f1eaae878d2d822d8c8fae1b215384b0d10
Diego Giacomelli <giacomelli@gmail.com>
2016-10-21 19:30:08 -0200

    Added Music label.

--- src/AudioShooter/Assets/_Scripts/Infrastructure/AudioConfig.cs
+++ src/AudioShooter/Assets/_Scripts/Infrastructure/AudioConfig.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioConfig : MonoBehaviour {
@@ -7,11 +8,13 @@ public class AudioConfig : MonoBehaviour {
 	public static AudioConfig Instance { get; private set; }
 	public AudioSource MusicSource { get; private set; }
 	public float MusicDuration { get; private set; }
+	public Text MusicLabel;
 
 	void Awake () {
 		MusicSource = GetComponent<AudioSource>();
 		MusicDuration = MusicSource.clip.length;
 		Instance = this;
-	}
 
+		MusicLabel.text = MusicSource.clip.name;
+	}
 }
