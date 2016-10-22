0d0824942d79355f8f1dc60d77a97a266144211d
Diego Giacomelli <giacomelli@gmail.com>
2016-10-22 16:58:20 -0200

    Created audio service console to improve debug.

---
+++ src/AudioShooter/Assets/_Scripts/Controllers/AudioServiceConsoleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioServiceConsoleController : MonoBehaviour {
+
+	public AudioServiceBase _audioService;
+	public Rect _area;
+
+	void OnGUI()
+	{
+		GUI.Window(1, _area, (id) =>
+		{
+			var metrics = _audioService.AudioBandBuffer;
+
+			for (int i = 0; i < AudioServiceBase.TotalBands; i++)
+			{
+				var metric = metrics[i];
+				GUILayout.Label("{0}: {1}".With(i, metric));
+				GUILayout.HorizontalSlider(metric, 0f, 1f);
+			}
+		}, "Audio Service console");
+	}
+}
