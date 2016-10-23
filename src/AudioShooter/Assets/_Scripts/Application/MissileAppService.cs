f82db76c397e9f6b320d16a9741f751d5bf2e53f
Diego Giacomelli <giacomelli@gmail.com>
2016-10-23 08:54:23 -0200

    Friendly fire support.

--- src/AudioShooter/Assets/_Scripts/Application/MissileAppService.cs
+++ src/AudioShooter/Assets/_Scripts/Application/MissileAppService.cs
@@ -2,12 +2,12 @@
 
 public static class MissileAppService 
 {
-	public static GameObject CreateMissile(GameObject shooter, Vector3 position, Vector3 direction, float velocity)
+	public static GameObject CreateMissile(GameObject shooter, Vector3 position, Vector3 direction, float velocity, bool friendlyFire = false)
 	{
 		var go = SHPoolsManager.GetGameObject("Missile");
 		go.transform.position = position;
 		var controller = go.GetComponent<MissileController>();
-		controller.Initialize(shooter, direction);
+		controller.Initialize(shooter, direction, friendlyFire);
 		controller._velocity = velocity;
 
 		return go;
--- src/AudioShooter/Assets/_Scripts/Controllers/MissileController.cs
+++ src/AudioShooter/Assets/_Scripts/Controllers/MissileController.cs
@@ -5,16 +5,18 @@ public class MissileController : MonoBehaviour {
 
 	Vector3 _target;
 	Vector3 _direction;
+	bool _friendlyFire;
 
 	public float _velocity;
 	public float _distance;
 
 	public GameObject Shooter { get; private set; }
 
-	public void Initialize(GameObject shooter, Vector3 direction)
+	public void Initialize(GameObject shooter, Vector3 direction, bool friendlyFire)
 	{
 		Shooter = shooter;
 		_direction = direction;
+		_friendlyFire = friendlyFire;
 		_target = transform.position + (_direction * _distance);
 	}
 
@@ -25,7 +27,9 @@ public class MissileController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		if ((other.IsEnemy() && other.gameObject != Shooter) || other.IsMountain())
+		var allowFriendlyFire = _friendlyFire || (this.Shooter.transform.IsEnemy() ^ other.IsEnemy());
+
+		if (allowFriendlyFire && ((other.IsEnemy() && other.gameObject != Shooter)) || other.IsMountain())
 		{
 			MissileAppService.DestroyMissile(gameObject);
 		}
