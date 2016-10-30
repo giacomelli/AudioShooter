220d5bb5d51b19c3b4cacfd91d817763e527567c
Diego Giacomelli <giacomelli@gmail.com>
2016-10-30 17:00:18 -0200

    Using CreateMissileTargetingSpaceship.

--- src/AudioShooter/Assets/_Scripts/Controllers/GroundEnemyController.cs
+++ src/AudioShooter/Assets/_Scripts/Controllers/GroundEnemyController.cs
@@ -2,24 +2,9 @@
 
 public class GroundEnemyController : EnemyControllerBase 
 {	
-	public bool _targetSpaceship;
-
 	protected override void PerformFire()
 	{
-		Vector3 direction;
-		float metric = BehaviourMetric;
-
-		if (_targetSpaceship)
-		{
-			direction = SpaceshipController.Instance.transform.position - transform.position;
-			direction = direction / direction.magnitude;
-		}
-		else
-		{
-			direction = transform.position.x < 0 ? Vector3.right : Vector3.left;
-		}
-
-		MissileAppService.CreateMissile(gameObject, transform.position, direction, _missileVelocity * metric);
+		MissileAppService.CreateMissileTargetingSpaceship(gameObject, _missileVelocity * BehaviourMetric);
 	}
 
 	protected override void PerformDestroy()
