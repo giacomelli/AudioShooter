18c1b82d83962f68a96377a5c31a8d31deff986f
Diego Giacomelli <giacomelli@gmail.com>
2016-10-30 09:04:02 -0200

    Using EnemyControllerBase.

--- src/AudioShooter/Assets/_Scripts/Controllers/MountainEnemyController.cs
+++ src/AudioShooter/Assets/_Scripts/Controllers/MountainEnemyController.cs
@@ -1,98 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 
-[RequireComponent(typeof(SoundConfig))]
-public class MountainEnemyController : SoundMonoBehaviour {
-	bool _isDead;
+public class MountainEnemyController : EnemyControllerBase
+{
 	Bounds _bounds;
-	bool _canFire = true;
-	Collider _collider;
 
-	public float _dieDelay;
-	public float _dieExplosionForce;
-
-	[Range(0f, 1f)]
-	public float _minAudioBandToFire;
-
-	public float _fireInterval;
-	public float _missileVelocity;
-
-	void Start()
+	protected override void PerformStart()
 	{
 		_bounds = GetComponent<MeshFilter>().mesh.bounds;
-		_collider = gameObject.GetComponent<Collider>();
 	}
 
-	void OnEnable()
+	protected override void PerformFire()
 	{
-		var rb = GetComponent<Rigidbody>();
+		var isFromLeftMountain = transform.position.x < 0;
+		var direction = isFromLeftMountain ? Vector3.right : Vector3.left;
+		var missilePosition = isFromLeftMountain ? transform.position + _bounds.center : transform.position - _bounds.center;
 
-		if (rb != null)
-		{
-			Destroy(rb);
-			_collider.enabled = true;
-		}
-
-		_isDead = false;
+		MissileAppService.CreateMissile(gameObject, missilePosition, direction, _missileVelocity);
 	}
 
-
-	void Update()
+	protected override void PerformDestroy()
 	{
-		if (BehaviourMetric >= _minAudioBandToFire)
-		{
-			Fire();
-		}
-	}
-
-	IEnumerator ReleaseFire()
-	{
-		yield return new WaitForSeconds(_fireInterval);
-		_canFire = true;
-	}
-
-	void Fire()
-	{
-		if (_canFire)
-		{
-			_canFire = false;
-			var isFromLeftMountain = transform.position.x < 0;
-			var direction = isFromLeftMountain ? Vector3.right : Vector3.left;
-			var missilePosition = isFromLeftMountain ? transform.position + _bounds.center : transform.position - _bounds.center;
-
-			MissileAppService.CreateMissile(gameObject, missilePosition, direction, _missileVelocity);
-			StartCoroutine(ReleaseFire());
-		}
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.IsMissile() && !other.IsEnemyMissile())
-		{
-			StartCoroutine(Die());
-		}
-	}
-
-	IEnumerator Die()
-	{
-		if (!_isDead)
-		{
-			Score.Instance.RegisterEnemyKilled();
-			StopCoroutine("ReleaseFire");
-			_isDead = true;
-			_collider.enabled = false;
-			var rb = gameObject.AddComponent<Rigidbody>();
-			rb.AddExplosionForce(_dieExplosionForce, Vector3.down, 10f);
-
-			yield return new WaitForSeconds(_dieDelay);
-
-			EnemyAppService.DestroyMountainEnemy(gameObject);
-		}
-	}
-
-	void MetricChanged()
-	{
-		Fire();
+		EnemyAppService.DestroyMountainEnemy(gameObject);
 	}
 }
