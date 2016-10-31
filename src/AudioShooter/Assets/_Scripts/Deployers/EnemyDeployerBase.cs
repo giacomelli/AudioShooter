5de4950feba54adbf8b5f1215962db8cf8f52883
Diego Giacomelli <giacomelli@gmail.com>
2016-10-30 22:10:01 -0200

    Created EnemyDeployerBase.

---
+++ src/AudioShooter/Assets/_Scripts/Deployers/EnemyDeployerBase.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SoundConfig))]
+public abstract class EnemyDeployerBase : SoundMonoBehaviour {
+	public float _minXDeploy;
+	public float _maxXDeploy;
+	[Range(0f, 1f)]
+	public float MinMetricToDeploy;
+
+	void Start()
+	{
+		AudioAnalysisService.Instance.SoundTick += delegate {
+			CreateWave();
+		};
+	}
+
+	void CreateWave()
+	{
+		var metric = CreationMetric;
+
+		if (metric >= MinMetricToDeploy)
+		{
+			var enemy = CreateEnemy();
+			var enemyX = _minXDeploy + (_maxXDeploy - _minXDeploy) * metric;
+
+			enemy.transform.position = new Vector3(
+				enemyX,
+				GetDeployY(),
+				AudioAnalysisService.Instance.Ticks);
+
+			enemy.GetComponent<SoundConfig>()._band = Config._band;
+		}
+	}
+
+	protected abstract GameObject CreateEnemy();
+
+	protected virtual float GetDeployY()
+	{
+		return 0;
+	}
+}
